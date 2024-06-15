b8d92b71abc7cbd59c1edd06f7ff864e4b41e667
shahRhyme007 <arifurrahmanrhyme@gmail.com>
2024-06-14 19:28:33 -0500

    start

---
+++ yolo_inference.py
@@ -0,0 +1,13 @@
+from ultralytics import YOLO 
+
+model = YOLO('yolov8x')
+
+
+# running the model on a video
+results = model.predict('input_videos/08fd33_4.mp4',save=True)
+print(results[0])
+print('=====================================')
+
+# running the model on the first frame of the video and go through the loop 
+for box in results[0].boxes:
+    print(box)
