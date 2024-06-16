eb5926522077bbe09f54cae6e2cbf2e010cc7677
shahRhyme007 <arifurrahmanrhyme@gmail.com>
2024-06-16 16:27:07 -0500

    done with the tracker function and saved in pickle file

--- trackers/tracker.py
+++ trackers/tracker.py
@@ -1,5 +1,7 @@
 from ultralytics import YOLO
 import supervision as sv
+import pickle
+import os
 
 
 
@@ -20,14 +22,31 @@ class Tracker:
         for i in range(0,len(frames),batch_size):
             detections_batch = self.model.predict(frames[i:i+batch_size],conf=0.1)
             detections += detections_batch
-            break
+            # break
         return detections
     
     
 
     #* get the object tracks from the frames given  
-    def get_object_tracks(self, frames):
+    def get_object_tracks(self, frames, read_from_stub=False, stub_path=None):
+
+        # if the tracks are already saved in a pickle file return them
+        if read_from_stub and stub_path is not None and os.path.exists(stub_path):
+            with open(stub_path,'rb') as f:
+                tracks = pickle.load(f)
+            return tracks
+
+
+
+
         detections = self.detect_frames(frames)
+
+        # initialize the track objects 
+        tracks={
+            "players":[], #dictionary of  each frame 
+            "referees":[],
+            "ball":[]
+        }
 
         # track the objects in the frames 
         for frame_num, detection in enumerate(detections):
@@ -35,6 +54,8 @@ class Tracker:
             cls_names_inv = {v:k for k,v in cls_names.items()}
             print(cls_names)
 
+
+            # Convert detections to supervision object
             detection_supervision = sv.Detections.from_ultralytics(detection)
 
             # Convert goalkeeper to player object
@@ -42,10 +63,58 @@ class Tracker:
                 if cls_names[class_id] == "goalkeeper":
                     detection_supervision.class_id[object_ind] = cls_names_inv["player"]
 
-            print(detection_supervision)
+            # track the objects in the frames 
+            detection_with_tracks = self.tracker.update_with_detections(detection_supervision)
 
-            break
 
+            # putting the output in a format so that we can utilize easily
+            tracks["players"].append({})
+            tracks["referees"].append({})
+            tracks["ball"].append({})
+
+
+            # looping through the detections of tracks
+            for frame_detection in detection_with_tracks:
+                # bounding box 
+                bbox = frame_detection[0].tolist()
+
+                # class id and track id
+                cls_id = frame_detection[3]
+                track_id = frame_detection[4]
+
+                
+                if cls_id == cls_names_inv['player']:
+                    tracks["players"][frame_num][track_id] = {"bbox":bbox}
+                
+                # referee is a special case because it is not a player
+                if cls_id == cls_names_inv['referee']:
+                    tracks["referees"][frame_num][track_id] = {"bbox":bbox}
+
+                
+            for frame_detection in detection_supervision:
+                bbox = frame_detection[0].tolist()
+                cls_id = frame_detection[3]
+
+                # ball is a special case
+                if cls_id == cls_names_inv['ball']:
+                    tracks["ball"][frame_num][1] = {"bbox":bbox}
+
+            # print(detection_supervision)
+            # print(detection_with_tracks)
+            
+            # for saving our tracks in a pickle file
+        if stub_path is not None:
+            with open(stub_path,'wb') as f:
+                pickle.dump(tracks,f)
+
+        return tracks
+
+    
+
+# ---------------------------------------------------------------#
+
+
+    
         
 
 
