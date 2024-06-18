1a4fe8183cbc2595a13b95c6b58b2b59faca0991
shahRhyme007 <arifurrahmanrhyme@gmail.com>
2024-06-17 19:10:55 -0500

    done getting colors from the player t-shirt

--- main.py
+++ main.py
@@ -2,6 +2,7 @@
 
 from utils import read_video, save_video
 from trackers import Tracker
+import cv2
 
 def main():
     #* Reading video
@@ -12,7 +13,14 @@ def main():
     tracks = tracker.get_object_tracks(video_frames, read_from_stub=True, stub_path="stubs/track_stubs.pkl")
 
 
+    #Just checking if the cropped image is working 
+    '''for track_id, player in tracks["players"][0].items():
+        bbox = player["bbox"]
+        frame = video_frames[0]
 
+        cropped_image = frame[int(bbox[1]):int(bbox[3]), int(bbox[0]):int(bbox[2])]
+        cv2.imwrite(f"output_videos/cropped_image.jpg", cropped_image)
+        break'''
 
     output_video_frames = tracker.draw_annotations(video_frames, tracks, team_ball_control=True)
     
